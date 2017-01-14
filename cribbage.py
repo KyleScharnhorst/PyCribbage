1d2f685b9b4f0914701bd4c99f2452ea916d2acf
KyleScharnhorst <scharkr1@uw.edu>
2017-01-13 22:36:19 -0800

    Change/Add: refactoring application to encapsulate more hand functionality

--- cribbage.py
+++ cribbage.py
@@ -217,12 +217,16 @@ class CardHand(object):
 	card_hand = None;
 	cut_card = None;
 	validity_checked = False;
-	validity = False;	
+	validity = False;
+	type_list = None;
+	score = 0	
 
 	# Initializer
 	def __init__(self, card_hand, cut_card):
 		self.card_hand = card_hand;
 		self.cut_card = cut_card;
+		self.init_type_list()
+		self.isValid()
 
 	# Takes a string like 1s,jh,3c,6d and builds and returns a hand object.
 	def create_hand(hand_str, cut_card):
@@ -265,17 +269,19 @@ class CardHand(object):
 		else:
 			print("Unexpected flush result, returning score of 0.")		
 		return 0;
+
+	def init_type_list(self):
+			self.type_list = list([0] * CardType.__len__())
+			for card in self.card_hand:
+				self.type_list[card.card_type.value] += 1
+			# Add cut card card into type list to check multiples
+			self.type_list[self.cut_card.card_type.value] += 1
 	
 	# Scores a hand for multiples (pairs, three of a kind, four of a kind)
 	# Returns the score from multiples or zero if no score.
 	def score_multiples(self):
 		if self.isValid():
 			score = 0
-			type_list = list([0] * CardType.__len__())
-			for card in self.card_hand:
-				type_list[card.card_type.value] += 1
-			# Add cut card card into type list to check multiples
-			type_list[self.cut_card.card_type.value] += 1
 			for ctype in range(len(type_list)):
 				if type_list[ctype] > 1:
 					# Have at least a pair, score
@@ -303,6 +309,24 @@ class CardHand(object):
 	
 	def score_fifteens(self):
 		raise NotImplementedError
+
+	# Checks the hand for jacks of the same suit as the cut card.
+	def score_nobs(self):
+		if self.isValid():
+			raise NotImplementedError
+
+	def score_hand(self):
+		self.score = 0
+		# Score knobs
+		self.score += self.score_nobs()
+		# Score multiples (pairs, three of a kind, four of a kind)
+		self.score += self.score_multiples()
+		# Score runs - min of three cards
+		self.score += self.score_runs()
+		# Score flush - is this a crib hand? (only if cut card doesn't match a hand that is a flush)
+		self.score += self.score_flush()
+		# Score fifteens
+		self.score += self.score_fifteens()
 					
 	# Determines if the hand is valid for processing
 	def isValid(self):
@@ -320,6 +344,10 @@ class CardHand(object):
 				return self.validity;
 			elif len(self.card_hand) != 4:
 				print("Error: Hand has more than 4 cards.")
+				self.validity = False;
+				return self.validity;
+			elif self.type_list == None:
+				print("Error: Type list is not initialized.")
 				self.validity = False;
 				return self.validity;
 			else:
@@ -361,15 +389,8 @@ def handle_score(score_input, cut_card):
 	# Check hand
 	if hand.isValid():
 		print("Scoring hand...");
-		score = 0;
-		# Score multiples (pairs, three of a kind, four of a kind)
-		score += hand.score_multiples()
-		# Score flush - is this a crib hand? (only if cut card doesn't match a hand that is a flush)
-		score += hand.score_flush()
-		# Score runs - min of three cards
-		# Score fifteens
-		
-		print("Total score: ", score)
+		hand.score_hand()
+		print("Total score: ", hand.score)
 
 # Main loop for application, continually asks for and handles input.
 def main():
