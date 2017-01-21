895e9fcbb67d0e79c7f7893df819ceaa859a14c3
KyleScharnhorst <scharkr1@uw.edu>
2017-01-21 02:52:54 -0800

    Add: output scores class.
    outputs the hand, cut card, and score of a hand to an output file.

---
+++ Misc/OutputScore.py
@@ -0,0 +1,28 @@
+import json
+from Cribbage import CardHand
+
+# This class contains functionality for outputting 
+# the scoring of cribbage hands to an output file.
+# Can be useful for reviewing past hands and creating
+# human verified test data.
+# Example JSON output
+# {"hand":["js","jh","jd","jc"],"cut_card":"ah","score":13}
+class OutputScore(object):
+    OUT_FILE_NAME = "OutputScores.txt"
+    HAND_STR = "hand"
+    CUT_CARD_STR = "cut_card"
+    SCORE_STR = "score"
+    
+    def Output(hand, score):
+        with open(OutputScore.OUT_FILE_NAME, "a") as out_file:
+            data = {}
+            # create card list that can be serialized
+            serializable_hand = list()
+            for card in hand.card_hand:
+                serializable_hand.append(card.toShortString())
+            data[OutputScore.HAND_STR] = serializable_hand
+            data[OutputScore.CUT_CARD_STR] = hand.cut_card.toShortString()
+            data[OutputScore.SCORE_STR] = score
+            json_result = json.dump(data, out_file)
+            #out_file.write(json_result)
+
