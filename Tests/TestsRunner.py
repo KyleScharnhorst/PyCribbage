da52469e4da44491852ff17d02e4a98336c954c2
KyleScharnhorst <scharkr1@uw.edu>
2017-01-21 00:16:18 -0800

    Setup test runner.

--- Tests/TestsRunner.py
+++ Tests/TestsRunner.py
@@ -2,4 +2,13 @@ from TestCribbage import *
 from TestHandScorer import *
 
 if __name__ == '__main__':
+    generate_card_test_list()
     unittest.main()
+    #test_suite = unittest.TestSuite()
+    #test_runner = unittest.TextTestRunner()
+    #test_suite.addTest(unittest.makeSuite(Test_atoi))
+    #test_suite.addTest(unittest.makeSuite(Test_Card))
+    #test_suite.addTest(unittest.makeSuite(Test_CardSuite))
+    #test_suite.addTest(unittest.makeSuite(Test_CardType))
+    #test_suite.addTest(unittest.makeSuite(Test_HandScorer))
+    #test_runner.run(test_suite)
