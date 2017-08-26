f8a2e0b02f96ae08a25a3a04eb0aada265a9723b
KyleScharnhorst <scharkr1@uw.edu>
2017-08-26 13:50:09 -0700

    Add: set debugging.
    other misc general cleanup.

--- Misc/Utils.py
+++ Misc/Utils.py
@@ -1,29 +1,36 @@
 
 # This module contains all utility functions
 
-# Recreation of ANSII C's atoi function.
-# Will create an int from a string even if there are is alpha characters in the string.
-# E.g.:
-# asd -> 0
-# 10c -> 10
-# 10c1 -> 10
-# 765 -> 765
 def atoi(a_str):
-	result = ""
-	for i in range(len(a_str)):
-		a_char = a_str[i]
-		if a_char >= '0' and a_char <= '9':
-			result = result + a_char
-		else:
-			break;
-	if len(result) > 0:
-		return int(result)
-	else:
-		return 0	 
+    """
+    # Recreation of ANSII C's atoi function.
+    # Will create an int from a string even if there are is alpha characters in the string.
+    # E.g.:
+    # asd -> 0
+    # 10c -> 10
+    # 10c1 -> 10
+    # 765 -> 765
+    """
+    result = ""
+    for i in range(len(a_str)):
+        a_char = a_str[i]
+        if a_char >= '0' and a_char <= '9':
+            result = result + a_char
+        else:
+            break
+    if len(result) > 0:
+        return int(result)
+    else:
+        return 0
 
 class Debug(object):
+    """Class used to encapsulate debugging variable(s)"""
     _Debugging = False
-    
+
+    @classmethod
+    def set_debugging(class_info, val):
+        Debug._Debugging = val
+
     @classmethod
     def Print(class_info, *args):
         if Debug._Debugging:
